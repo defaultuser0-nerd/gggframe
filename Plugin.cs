48af31c5b4ca27e480a42767dfefa3abb7d3704a
defaultuser0 <axolotlslmao@gmail.com>
2024-12-25 16:52:20 -0600

    gasdihfjlk

---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace AssetBundleTemplate
+{
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		public static Plugin instance;
+        public static AssetBundle bundle;
+        public static GameObject assetBundleParent;
+        public static string parentName = "ggg";
+        void Start()
+		{
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+		}
+		void OnGameInitialized()
+		{
+			instance = this;
+            bundle = LoadAssetBundle("ggg.AssetBundles.ggg");
+            assetBundleParent = Instantiate(bundle.LoadAsset<GameObject>(parentName));
+            assetBundleParent.transform.position = new Vector3(-67.2225f, 11.575f, -82.6090f);
+        }
+        public AssetBundle LoadAssetBundle(string path)
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
+            stream.Close();
+            return bundle;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace AssetBundleTemplate
+{
+    class PluginInfo
+    {
+        public const string GUID = "defaultuser0.ggg";
+        public const string Name = "ggg";
+        public const string Version = "1.0.0";
+    }
+}
